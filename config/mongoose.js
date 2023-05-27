d7bd704123b3ee0fc564e61d323a43f0f2db5ea1
RitikaRathorhub812 <128072113+RitikaRathorhub812@users.noreply.github.com>
2023-05-27 13:56:45 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// storing the db on mongo atlas
+const DB = "mongodb+srv://RitikaRathor:Ritika1461999@habittracker.4ldjnxu.mongodb.net";
+
+// mongoose.connect('mongodb://127.0.0.1/habit_tracker');
+
+mongoose.connect(DB).then(()=>{
+    console.log('Connection successful!');
+}).catch((err) => console.log("no connection " + err));
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
